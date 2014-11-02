12da73d9d5ff6566b57278b194eb465fd7bf4675
wyicwx <wyicwx@gmail.com>
2014-11-02 19:58:44 +0800

    up

--- example/bonefile.js
+++ example/bonefile.js
@@ -1,42 +1,54 @@
-var bone = require('../index.js');
+var bone = require('bone');
 
+var filesDir = bone.define('./files');
 // define ~/bonetmp copy from ~/src
-bone.define('~/bonetmp')
-	.src('~/src/*.js')
+filesDir.define('./bonetmp')
+	.src('./src/**/*.js')
 
-// define combine file
-bone.define('~/bonetmp/vfile.js')
-	.src(combine([
-		'~/a.js',
-		'~/b.js'
-	]))
-
-// define combine file
-bone.define('~/bonetmp/vfile.js')
-	.src(combine([
-		'~/a.js',
-		'~/b.js'
-	]))
-
-bone.define('~/bonetmp/')
-	.src('~/src/*.js')
-	.rename(function() {
-
+filesDir.define('./bonetmp')
+	.src('./src/**/*.js')
+	.rename(function(name) {
+		return '_tmp_'+name;
 	});
 
-bone.define('~/')
+filesDir.define('./bonetmp/abc.js')
+	.src('./src/models/a.js')
 
-bone.define('~/bonetmp')
-	.src('~/src/*.js')
-	.act(include())
-	.act();
+filesDir.define('./dist/index.js')
+	.src('./bonetmp/models/a.js');
 
-bone.define('~/bonetmp')
-	.src('~/src/**/*.less')
-	.act(less());
+// define combine file
+// bone.define('~/bonetmp/vfile.js')
+// 	.src('./abc.js')
+// 	.act(combine([
+// 		'~/a.js',
+// 		'~/b.js'
+	// ]));
 
-bone.define('~/bonetmp/base64')
-	.src('~/src/**/*.png')
-	.act(base64());
+// define combine file
+// bone.define('~/bonetmp/vfile.js')
+// 	.src(combine([
+// 		'~/a.js',
+// 		'~/b.js'
+// 	]))
 
-bone.define('~/')
+// bone.define('~/bonetmp')
+// 	.src('~/src/*.js')
+// 	.act(include())
+// 	.act();
+
+// bone.define('~/bonetmp')
+// 	.src('~/src/**/*.less')
+// 	.act(less());
+
+// bone.define('~/bonetmp')
+// 	.src('~/src/**/*.[png|jpg|gif|jpeg]')
+// 	.act(base64())
+// 	.rename(function(filename) {
+// 		return filename.replace(/(png|jpg|gif|jpeg)$/, 'base64');
+// 	});
+
+var tmpDir = bone.define('~/bonetmp');
+
+tmpDir.define('file.js')
+	
