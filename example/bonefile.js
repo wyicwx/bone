4ccbcfbf79674d4ca85a662c653fa924759969bc
wyicwx <wyicwx@gmail.com>
2014-11-05 16:01:54 +0800

    up

--- example/bonefile.js
+++ example/bonefile.js
@@ -1,4 +1,7 @@
 var bone = require('bone');
+var combine = require('bone-combine');
+var include = require('bone-include');
+var connect = require('bone-connect');
 
 var filesDir = bone.define('./files');
 // define ~/bonetmp copy from ~/src
@@ -12,43 +15,58 @@ filesDir.define('./bonetmp')
 	});
 
 filesDir.define('./bonetmp/abc.js')
-	.src('./src/models/a.js')
+	.src('./src/models/a.js');
 
 filesDir.define('./dist/index.js')
 	.src('./bonetmp/models/a.js');
 
 // define combine file
-// bone.define('~/bonetmp/vfile.js')
-// 	.src('./abc.js')
-// 	.act(combine([
-// 		'~/a.js',
-// 		'~/b.js'
-	// ]));
-
+bone.define('~/bonetmp/vfile.js')
+	.src('./abc.js')
+	.act(combine([
+		'~/a.js',
+		'~/b.js'
+	]));
+// (((abc)+a+b)+c+d) 
 // define combine file
-// bone.define('~/bonetmp/vfile.js')
-// 	.src(combine([
-// 		'~/a.js',
-// 		'~/b.js'
-// 	]))
+bone.define('~/bonetmp/vfile.js')
+	.src('./abc.js')
+	.act(combine([
+		'~/a.js',
+		'~/b.js'
+	]))
+	.act(combine([
+		'~/c.js',
+		'~/d.js'
+	]));
 
-// bone.define('~/bonetmp')
-// 	.src('~/src/*.js')
-// 	.act(include())
-// 	.act();
+bone.define('~/bonetmp')
+	.src('~/src/*.js')
+	.act(include())
+	.act();
 
-// bone.define('~/bonetmp')
-// 	.src('~/src/**/*.less')
-// 	.act(less());
+bone.define('~/bonetmp')
+	.src('~/src/**/*.less')
+	.act(less());
 
-// bone.define('~/bonetmp')
-// 	.src('~/src/**/*.[png|jpg|gif|jpeg]')
-// 	.act(base64())
-// 	.rename(function(filename) {
-// 		return filename.replace(/(png|jpg|gif|jpeg)$/, 'base64');
-// 	});
+bone.define('~/bonetmp')
+	.src('~/src/**/*.[png|jpg|gif|jpeg]')
+	.act(base64())
+	.rename(function(filename) {
+		return filename.replace(/(png|jpg|gif|jpeg)$/, 'base64');
+	});
 
-var tmpDir = bone.define('~/bonetmp');
+var combineInlcude = bone.wrapper(combine(),
+	include());
+
+bone.define('~/bonetmp')
+	.src('~/src/a.js')
+	.act(combineInlcude());
+
+
+connect(bone);
+proxy(bone);
+// var tmpDir = bone.define('~/bonetmp');
 
 tmpDir.define('file.js')
 	
